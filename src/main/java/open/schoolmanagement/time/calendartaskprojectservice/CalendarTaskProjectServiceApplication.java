4613a797c7121c301167c990b7aa6cc68d08cd6b
Philipp <philippkostyra@gmail.com>
2018-05-26 18:20:58 +0200

    Initial Spring Boot Setup
    Copy of Initializr Content

---
+++ src/main/java/open/schoolmanagement/time/calendartaskprojectservice/CalendarTaskProjectServiceApplication.java
@@ -0,0 +1,12 @@
+package open.schoolmanagement.time.calendartaskprojectservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarTaskProjectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarTaskProjectServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/open/schoolmanagement/time/calendartaskprojectservice/CalendarTaskProjectServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package open.schoolmanagement.time.calendartaskprojectservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalendarTaskProjectServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
