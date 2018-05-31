9246d1a012b43a13c4a3ae21257e06d8afe13c6b
Philipp <philippkostyra@gmail.com>
2018-05-31 20:34:38 +0200

    ExceptionHandling
    - Exception Handler
    - First custom Exceptions

---
+++ src/main/java/open/schoolmanagement/time/calendartaskprojectservice/exception/ExceptionResponse.java
@@ -0,0 +1,48 @@
+/*
+   Copyright 2018 Open School Management
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+package open.schoolmanagement.time.calendartaskprojectservice.exception;
+
+import java.util.Date;
+
+
+public class ExceptionResponse {
+  private Date timestamp;
+  private String message;
+  private String details;
+
+  /**
+   * ExceptionResponse: convert the Exception into a externalised form
+   * @param timestamp Timestamp when the exception happend
+   * @param message What went wrong!
+   * @param details Details of the error
+   */
+  public ExceptionResponse(Date timestamp, String message, String details) {
+    super();
+    this.timestamp = timestamp;
+    this.message = message;
+    this.details = details;
+  }
+
+  public Date getTimestamp() {
+    return timestamp;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public String getDetails() {
+    return details;
+  }
+}
