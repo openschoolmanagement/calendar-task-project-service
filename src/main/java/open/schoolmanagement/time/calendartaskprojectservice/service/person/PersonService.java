7bf50267cd42c76c563d4368e46406ae5b77a6ad
Philipp <philippkostyra@gmail.com>
2018-08-26 22:54:03 +0200

    Unit Tests

--- src/main/java/open/schoolmanagement/time/calendartaskprojectservice/service/person/PersonService.java
+++ src/main/java/open/schoolmanagement/time/calendartaskprojectservice/service/person/PersonService.java
@@ -1,14 +1,11 @@
 package open.schoolmanagement.time.calendartaskprojectservice.service.person;
 
 import java.util.Optional;
-import java.util.concurrent.Callable;
 import open.schoolmanagement.time.calendartaskprojectservice.domain.person.Person;
 import open.schoolmanagement.time.calendartaskprojectservice.repository.person.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Scheduler;
 
 @Service
 public class PersonService {
@@ -16,17 +13,16 @@ public class PersonService {
 
   private PersonRepository personRepository;
 
-  @Qualifier("jdbcScheduler")
+
   @Autowired
-  private Scheduler scheduler;
+  private PersonService(PersonRepository personRepository) {
+    this.personRepository = personRepository;
+  }
+
 
   public Mono<Optional<Person>> findById(Long personId) {
-    return async(() -> personRepository.findById(personId));
+    return Mono.just(personRepository.findById(personId));
   }
 
-
-  private <T> Mono<T> async(Callable<T> callable) {
-    return Mono.fromCallable(callable).publishOn(scheduler);
-  }
 
 }
